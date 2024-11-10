c1b40ff0e5626706f1653cd831fd01da4738ea51
KhuePM <minhkhuepham99@gmail.com>
2024-11-10 21:45:52 +0700

    Initial Commit

---
+++ Task Management/Task_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementApp.swift
+//  Task Management
+//
+//  Created by KhuePM on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
